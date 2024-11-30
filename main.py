c98d8f74873f74198f9f39e7ac4a0660c9d6b5b2
hao-to <th.nguyen77@gmail.com>
2024-11-30 22:43:59 +0100

    added main() function and structured entry point

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import store
 import products
 
-# setup initial stock of inventory
-product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
-                products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
-                products.Product("Google Pixel 7", price=500, quantity=250)
-                ]
-
-best_buy = store.Store(product_list)
-
 
 def create_shopping_list(store_instance):
     """
@@ -110,4 +102,18 @@ def start(store_instance):
             break
 
 
-start(best_buy)
+def main():
+    # setup initial stock of inventory
+    product_list = [products.Product("MacBook Air M2", price=1450, quantity=100),
+                    products.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    products.Product("Google Pixel 7", price=500, quantity=250)
+                    ]
+
+    best_buy = store.Store(product_list)
+
+    # start the program
+    start(best_buy)
+
+
+if __name__ == "__main__":
+    main()
